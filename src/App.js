f5f01ccc136b9a5b500d954ca71719b42f5a5999
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-10-06 11:22:35 -0300

    trabalhando com state

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
 import React, {Component} from 'react';
 
- class Equipe extends Component {
+ class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            nome: 'James',
+            contador:0
+        };
+        this.aumentar = this.aumentar.bind(this);
+        this.diminuir = this.diminuir.bind(this);
+
+    }
+    aumentar(){
+        let state = this.state;
+        state.contador +=1;
+        this.setState(state);
+    }
+    diminuir(){
+        let state = this.state;
+        state.contador -=1;
+        this.setState(state);
+    }
     render(){
         return (
             <div>
-                <Sobre nome={this.props.nome} cargo={this.props.cargo} idade={this.props.idade}/>
+                <h1>Contador</h1>
+                <h3>
+                    <button onClick={this.diminuir}>-</button>
+                    {this.state.contador}
+                    <button onClick={this.aumentar}>+</button>
+                </h3>
             </div>
         );
     }
 }
 
-class Sobre extends Component {
-    render(){
-        return (
-            <div>
-                <h2>Olá sou o {this.props.nome}</h2>
-                <h2>cargo: {this.props.cargo}</h2>
-                <h2>idade: {this.props.idade}</h2>
-            </div>
-        );
-    }
-}
 
-
-function App(){
-    return <div>
-            <Equipe nome="James" cargo="Desenvolvedor" idade="27" facebook="https://www.facebook.com/in/jamessonsena/" linkedin="https://www.linkedin.com/in/jamessonsena/"/>
-
-        </div>
-}
 export default App;
