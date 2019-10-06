c3c3c64c45a6f4d03a6221381a7d966a4188204d
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-10-06 00:24:02 -0300

    usando dois componentes de class

--- src/App.js
+++ src/App.js
@@ -2,25 +2,27 @@ import React, {Component} from 'react';
 
  class Equipe extends Component {
     render(){
-    return (
-        <div>
-            <h2>Olá sou o {this.props.nome}</h2>
-            <h2>cargo: {this.props.cargo}</h2>
-            <h2>idade: {this.props.idade}</h2>
-        </div>
-
+        return (
+            <div>
+                <Sobre nome={this.props.nome} cargo={this.props.cargo} idade={this.props.idade}/>
+            </div>
         );
     }
 }
-const Social = (props) => {
-    return (
-        <div>
-            <a href={props.linkedin}>Linkedin </a>
-            <a href={props.facebook}>Facebook </a>
-        </div>
 
+class Sobre extends Component {
+    render(){
+        return (
+            <div>
+                <h2>Olá sou o {this.props.nome}</h2>
+                <h2>cargo: {this.props.cargo}</h2>
+                <h2>idade: {this.props.idade}</h2>
+            </div>
         );
+    }
 }
+
+
 function App(){
     return <div>
             <Equipe nome="James" cargo="Desenvolvedor" idade="27" facebook="https://www.facebook.com/in/jamessonsena/" linkedin="https://www.linkedin.com/in/jamessonsena/"/>
