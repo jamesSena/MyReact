e12c585bd0808140532a88cb1c82ccd85d579a4c
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-10-05 22:43:53 -0300

    trabalhando com props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-Olá mundo        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const BemVindo = (props) => <h2>Bem-Vindo {props.nome}</h2>
+const BemVindo2 = (props) => {
+    return (
+        <div>
+            <h2>Nome: {props.nome} </h2>       
+            <h3>Idade: {props.idade}</h3>      
+        </div>
+
+        );
 }
 
+function App(){
+    return <div>
+            <BemVindo nome="James"/>
+            <BemVindo2 nome="James" idade="27"/>
+        </div>
+}
 export default App;
