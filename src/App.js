994dcb6ab7e120ebc12f5815fb3f5e4ba32a71a4
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-10-05 23:59:06 -0300

    Adionando mais um componte, para midias sociais

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ const Equipe = (props) => {
                     cargo={props.cargo}
                     idade={props.idade}
                 />
+                <hr/>
+            <Social
+                linkedin={props.linkedin}
+                facebook={props.facebook}
+
+                />
         </div>
 
         );
@@ -15,14 +21,27 @@ const Sobre = (props) => {
     return (
         <div>
             <h2>Olá sou o {props.nome}</h2>
+            <h2>cargo: {props.cargo}</h2>
+            <h2>idade: {props.idade}</h2>
+
         </div>
 
         );
 }
 
+const Social = (props) => {
+    return (
+        <div>
+            <a href={props.linkedin}>Linkedin </a>
+            <a href={props.facebook}>Facebook </a>
+        </div>
+
+        );
+}
 function App(){
     return <div>
-            <Equipe nome="James" cargo="Desenvolvedor" idade="27"/>
+            <Equipe nome="James" cargo="Desenvolvedor" idade="27" facebook="https://www.facebook.com/in/jamessonsena/" linkedin="https://www.linkedin.com/in/jamessonsena/"/>
+
         </div>
 }
 export default App;
